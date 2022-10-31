87c67239c771e84141882bac67e941508de7f035
QI-Joe <80494923+QI-Joe@users.noreply.github.com>
2022-10-31 16:19:15 +0800

    First Try

--- Game/Controller/Controller.py
+++ Game/Controller/Controller.py
@@ -1,32 +1,43 @@
-from Game.Model import Model
+from Game.Model import Players as player
+import Game.Model.Model as model
 from Game.View import View
 import time
 
 
 class Controller:
 
-    def __init__(self, game_model: Model, game_view: View):
+    def __init__(self, game_model: model.Model, game_view: View):
         self.game_model = game_model
         self.game_view = game_view
 
     def chooseLanguage(self):
         inputs: str = input(
             "Please choose your language preference(Chinese or English), and color (Green or Blue): ")
-
         if inputs == "chinese":
             pass
 
         elif inputs == "english":
             pass
 
-    def chooseSide(self):
-        inputs: str = input("Please choose your preferred side: ")
-
+    def chooseSide(self) -> list:
+        """
+        so after choosing the side, two player list should with a signature to be pointed and able
+        to be recognized.
+        Major related function should get from Model.Model
+        :return: None
+        """
+        inputs: str = input("Please choose your preferred side: ")  # how is choosing?
+        receiver: list = model.Model.getChessboard()
+        riverpos: list = [(x, y) for x in [1, 2, 4, 5] for y in range(3, 6)]
+        trappos: list = [(2, 0), (3, 1), (4, 0), (2, 8), (3, 7), (4, 8)]
+        denpos: list = [(3, 0), (3, 8)]
         if inputs == "down":
-            pass
-
+            receiver[0] = player.Players(chessList=[], riverPos=riverpos, trapPos=trappos[0:3], denPos=denpos[0])
+            receiver[1] = player.Players(chessList=[], riverPos=riverpos, trapPos=trappos[3:6], denPos=denpos[1])
         elif inputs == "up":
-            pass
+            receiver[1] = player.Players(chessList=[], riverPos=riverpos, trapPos=trappos[0:3], denPos=denpos[0])
+            receiver[0] = player.Players(chessList=[], riverPos=riverpos, trapPos=trappos[3:6], denPos=denpos[1])
+        return receiver
 
     def executeInput(self, turnFlag):
 
@@ -34,7 +45,7 @@ class Controller:
             "Please input your commands: ").split()
 
         if inputs[0] == "move":
-            self.checkMove()
+            self.checkMove(turnFlag)
             pass
         elif inputs[0] == "help":
             self.game_view.printHelp()
@@ -48,8 +59,9 @@ class Controller:
         else:
             Exception("Input wrong, system will stop")
 
-    def checkMove():
-        pass
+    def checkMove(self, turnFlag):
+
+        ...
 
     def getHelp():
         pass
@@ -75,8 +87,8 @@ class Controller:
     def time_spire():
         available_second = 60
         for i in range(1, available_second):
-            print('you have %d seconds' % (available_second-i))
-            if available_second-i == 10:
+            print('you have %d seconds' % (available_second - i))
+            if available_second - i == 10:
                 print("10 seconds left.")
         print("Time expired, your turn finished.")
         time.sleep(1)  # 时间倒计时
--- Game/Model/Players.py
+++ Game/Model/Players.py
@@ -7,3 +7,10 @@ class Players():
         self.riverPos = riverPos
         self.trapPos = trapPos
         self.denPos = denPos
+
+    riverpos: list = [(x, y) for x in [1, 2, 4, 5] for y in range(3, 6)]
+    trappos: list = [(2, 0), (3, 1), (4, 0), (2, 8), (3, 7), (4, 8)]
+    denpos: list = [(3, 0), (3, 8)]
+
+    # receiver[0] = player.Players(chessList=[], riverPos=riverpos, trapPos=trappos[0:3], denPos=denpos[0])
+    # receiver[1] = player.Players(chessList=[], riverPos=riverpos, trapPos=trappos[3:6], denPos=denpos[1])
