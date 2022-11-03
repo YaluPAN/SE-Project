c246d20fc52e02c5e8c4a943b8f1ba067c8fd2b5
Melvin-King <79367726+Melvin-King@users.noreply.github.com>
2022-11-04 07:42:25 +0800

    Update Jungle.py

--- Jungle.py
+++ Jungle.py
@@ -4,7 +4,11 @@ from Game.View import View
 from Game.Model import Model
 from Game.Controller import Controller
 
-
+'''
+Initialization for the Jungle object which includes all the essential objects for the whole game.
+This class includes two players defined in Model, a view and a model object, and a controller as the kernel part to conduct the game.
+A boolean variable inturn is defined.
+'''
 class Jungle:
     def __init__(self):
         self.initPlayer1 = Model.Players
@@ -14,7 +18,10 @@ class Jungle:
         self.game_controller = Controller.Controller(
             self.game_model, self.game_view)
         self.inturn = False
-
+'''
+Turnflag counts the number of rounds for a started game. Taking it modulo 2 will we obtain the boolean input for parameter in executeInput().
+Call the ifEnd() function to judge whether the game ends in every round.
+'''
     def start(self):
         turnFlag = 0
         self.game_controller.chooseLanguage()
@@ -25,7 +32,9 @@ class Jungle:
             turnFlag += 1
             Controller.executeInput(turnFlag % 2)
 
-
+'''
+main function, executes once jungle.py executes.
+'''
 def main():
     sys = Jungle()
     sys.start()
